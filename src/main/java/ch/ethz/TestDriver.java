42080327347ed311df63db75683b2585c72fbf6d
Stevan Mihajlovic <mstevan@student.ethz.ch>
2020-10-28 17:25:45 +0100

    Pushing test-case query to Rumble

--- src/main/java/ch/ethz/TestDriver.java
+++ src/main/java/ch/ethz/TestDriver.java
@@ -3,26 +3,35 @@ package ch.ethz;
 import net.sf.saxon.s9api.*;
 import net.sf.saxon.s9api.streams.Predicates;
 import net.sf.saxon.s9api.streams.Steps;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.rumbledb.api.Item;
 import org.rumbledb.api.Rumble;
 import org.rumbledb.api.SequenceOfItems;
 import org.rumbledb.config.RumbleRuntimeConfiguration;
+import sparksoniq.spark.SparkSessionManager;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class TestDriver {
     private String testsRepositoryScriptFileName = "get-tests-repository.sh";
     private String catalogFileName = "catalog.xml";
     private Path workingDirectoryPath;
     private Path testsRepositoryDirectoryPath;
-    private String testSetToTest = "";
+    private String testSetToTest = "math-acos.xml";
+    private SparkSession sparkSession;
+    private Rumble rumbleInstance;
 
     void execute() {
         workingDirectoryPath = Paths.get("").toAbsolutePath();
         getTestsRepository();
+        initializeSparkAndRumble();
 
         try {
             processCatalog(new File(testsRepositoryDirectoryPath.resolve(catalogFileName).toString()));
@@ -65,6 +74,22 @@ public class TestDriver {
         System.out.println("Tests repository obtained!");
     }
 
+    private void initializeSparkAndRumble() {
+        // Initialize Spark
+        sparkSession = SparkSession.builder()
+                .master("local")
+                .appName("rumble-test-suite")
+                .getOrCreate();
+
+        // Initialize Rumble
+        // TODO RumbleRuntimeConfiguration rumbleConf = new RumbleRuntimeConfiguration() // I added init() in default ctor
+        RumbleRuntimeConfiguration rumbleConf = new RumbleRuntimeConfiguration(
+                new String[]{
+                        "--output-format","json"
+                });
+        rumbleInstance = new Rumble(rumbleConf);
+    }
+
     private void processCatalog(File catalogFile) throws SaxonApiException {
         // TODO: Different initialization from Saxon, check if it is okay
         Processor testDriverProcessor = new Processor(false);
@@ -104,28 +129,43 @@ public class TestDriver {
         File testSetFile = new File(testsRepositoryDirectoryPath.resolve(testSetFileName).toString());
         XdmNode testSetDocNode = catalogBuilder.build(testSetFile);
 
-        for (XdmNode testCase : testSetDocNode.select(Steps.descendant("test-case")).asList()) {
-            this.processTestCase(testCase, xpc);
+        // TODO Think about scenario in which we just want to run a specified TestSet (like math) and not all of them
+        if (testSetToTest.equals("") || testSetFileName.endsWith(testSetToTest)) {
+            for (XdmNode testCase : testSetDocNode.select(Steps.descendant("test-case")).asList()) {
+                this.processTestCase(testCase, xpc);
+
+            }
         }
     }
 
     private void processTestCase(XdmNode testCase, XPathCompiler xpc){
-        // TODO Think about scenario in which we just want to run a specified TestSet (like math) and not all of them
-        if (testSetToTest.equals("") || testCase.attribute("name").equals(testSetToTest)) {
-            XdmNode resultElement = testCase.select(Steps.child("result")).asNode();
-            XdmNode testElement = testCase.select(Steps.child("test")).asNode();
-            Rumble instance = new Rumble(new RumbleRuntimeConfiguration());
-            SequenceOfItems result = instance.runQuery(testElement.toString());
+        XdmNode resultNode = testCase.select(Steps.child("result")).asNode();
+        XdmNode testNode = testCase.select(Steps.child("test")).asNode();
+
+        String testString = testNode.getStringValue();
+
+        // Small converter
+        testString = Convert(testString);
+
+        // Execute query
+        SequenceOfItems queryResult = rumbleInstance.runQuery(testString);
+
+        List<Item> resultList = new ArrayList<Item>(){};
+        queryResult.populateListWithWarningOnlyIfCapReached(resultList);
+
+        while (queryResult.hasNext()){
+            Item item = queryResult.next();
+        }
 
 
-            boolean needSerializedResult = resultElement.select(Steps.descendant("assert-serialization-error")).exists() || resultElement.select(Steps.descendant("serialization-matches")).exists();
-            boolean needResultValue = needSerializedResult &&
-                    resultElement.select(Steps.descendant(Predicates.isElement())
-                            .where(Predicates.not(Predicates.hasLocalName("serialization-matches")
-                                    .or(Predicates.hasLocalName("assert-serialization-error"))
-                                    .or(Predicates.hasLocalName("any-of"))
-                                    .or(Predicates.hasLocalName("all-of")))))
-                            .exists();
+        boolean needSerializedResult = resultNode.select(Steps.descendant("assert-serialization-error")).exists() || resultNode.select(Steps.descendant("serialization-matches")).exists();
+        boolean needResultValue = needSerializedResult &&
+                resultNode.select(Steps.descendant(Predicates.isElement())
+                        .where(Predicates.not(Predicates.hasLocalName("serialization-matches")
+                                .or(Predicates.hasLocalName("assert-serialization-error"))
+                                .or(Predicates.hasLocalName("any-of"))
+                                .or(Predicates.hasLocalName("all-of")))))
+                        .exists();
 
 //        dependency = (XdmNode)var17.next();
 //        exp = dependency.attribute("type");
@@ -145,6 +185,21 @@ public class TestDriver {
 //                return;
 //            }
 //        }
-        }
+    }
+
+    private String Convert(String testString) {
+
+        // Found in fn/abs.xml
+        testString = testString.replace("xs:integer","integer");
+        testString = testString.replace("xs:int","integer");
+        testString = testString.replace("xs:double", "double");
+
+        // Found in math/math-acos.xml
+        testString = testString.replace("math:","");
+
+        // Replace with regex checks!
+        testString = testString.replace("fn:","");
+
+        return testString;
     }
 }
