3c2e666181ce949917570e7cab1df79afd561bc0
mstevan <mstevan@student.ethz.ch>
2021-03-12 18:36:11 +0100

    Code Snippets nice formatting

--- thesis/source_code_without_results.java
+++ thesis/source_code_without_results.java
@@ -2,7 +2,7 @@ private boolean AssertEq(List<Item> resultAsList,
         XdmNode assertion) throws UnsupportedTypeException {
     String assertExpression = assertion.getStringValue();
     List<String> lines = resultAsList.stream()
-        .map(x -> x.serialize()).collect(Collectors.toList());
+       .map(x -> x.serialize()).collect(Collectors.toList());
     assertExpression += "=" + lines.get(0);
     List<Item> nestedResult = runQuery(assertExpression);
     return AssertTrue(nestedResult);
@@ -12,6 +12,6 @@ private boolean AssertStringValue(List<Item> resultAsList,
         XdmNode assertion) throws UnsupportedTypeException{
     String assertExpression = assertion.getStringValue();
     List<String> lines = resultAsList.stream()
-        .map(x -> x.serialize()).collect(Collectors.toList());
+       .map(x -> x.serialize()).collect(Collectors.toList());
     return assertExpression.equals(String.join(" ",lines));
 }
