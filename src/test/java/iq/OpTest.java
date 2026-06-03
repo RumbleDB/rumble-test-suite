7d6339ab89d63a4c3f87cffa18c3a30bfaa5cf89
Jimmy Cai <jimmy@cai.im>
2026-06-03 17:37:45 +0200

    Remove timeout annotations from test methods and configure default timeout settings to 10 minutes

--- src/test/java/iq/OpTest.java
+++ src/test/java/iq/OpTest.java
@@ -2,11 +2,9 @@ package iq;
 
 import evaluation.CollectedTestCase;
 import iq.base.TestBase;
-import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class OpTest extends TestBase {
@@ -16,7 +14,6 @@ public class OpTest extends TestBase {
 
     @ParameterizedTest(name = "{0}")
     @MethodSource("data")
-    @Timeout(value = 1000000, unit = TimeUnit.MILLISECONDS, threadMode = Timeout.ThreadMode.SEPARATE_THREAD)
     public void test(CollectedTestCase testCase) {
         testCase(testCase);
     }
